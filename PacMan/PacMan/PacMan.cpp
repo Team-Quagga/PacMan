7e5be8917957020a0f616b4c28f606cef78b54e4
unknown <hapb12@H429-0002.students.ad.bth.se>
2014-02-25 15:47:47 +0100

    Fixade init av glfw och glew main loop i pacman.cpp
    Signed-off-by: unknown <hapb12@H429-0002.students.ad.bth.se>

--- PacMan/PacMan/PacMan.cpp
+++ PacMan/PacMan/PacMan.cpp
@@ -3,6 +3,22 @@
 
 int main()
 {
-	std::cout<<"Hej GitHub! "<<std::endl;
+
+	GLFWwindow* window = Init("Pacman", 1280, 720);
+	ScreenManager manager;
+
+	while(!glfwWindowShouldClose(window))
+	{
+		/* update */
+		manager.Update();
+		/* Draw */
+		manager.Draw();
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
 	return 0;
 }
